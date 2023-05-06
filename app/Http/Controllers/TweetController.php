8b98e09bc9addc8ad7c1fcc4202e8900a6ab4681
RishikantSri <65564464+RishikantSri@users.noreply.github.com>
2023-05-06 16:02:52 +0530

    store method added

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tweet;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
@@ -30,9 +31,16 @@ class TweetController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->tweets()->create($validated);
+ 
+        return redirect(route('tweets.index'));
     }
 
     /**
